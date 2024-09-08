b543750c49a234654d140ff076d463b66cf31bd8
Ian McNaughton <IanMcNaugh@gmail.com>
2024-09-08 13:52:10 -0500

    testing and playing around

--- src/color.rs
+++ src/color.rs
@@ -2,6 +2,7 @@
 ///
 /// This module contains the Color enum, which represents the color of either a chess piece or a
 /// square on the board.
+#[derive(Clone, Copy)]
 pub enum Color {
     White,
     Black,
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,39 @@ use chess::chess_piece::{ChessPiece, PieceType};
 fn main() {
     println!("Hello, world!");
 
-    let white_pawn = ChessPiece::new(Color::White, PieceType::Pawn);
-    let black_queen = ChessPiece::new(Color::Black, PieceType::Queen);
+    let white_pieces = create_piece_set(Color::White);
+    let black_pieces = create_piece_set(Color::Black);
 
-    println!("{}", white_pawn);
-    println!("{}", black_queen);
+    let mut empty_board = [[None; 8]; 8];
+
+    let mut square_a1 = empty_board[0][0];
+
+    square_a1 = Some(&white_pieces[0]);
+
+    for piece in white_pieces {
+        println!("{}", piece);
+    }
+
+    for piece in black_pieces {
+        println!("{}", piece);
+    }
+}
+
+fn create_piece_set(color: Color) -> Vec<ChessPiece> {
+    let mut pieces = Vec::new();
+    for _ in 0..8 {
+        pieces.push(ChessPiece::new(color, PieceType::Pawn));
+    }
+    for _ in 0..2 {
+        pieces.push(ChessPiece::new(color, PieceType::Rook));
+        pieces.push(ChessPiece::new(color, PieceType::Knight));
+        pieces.push(ChessPiece::new(color, PieceType::Bishop));
+    }
+    pieces.push(ChessPiece::new(color, PieceType::Queen));
+    pieces.push(ChessPiece::new(color, PieceType::King));
+    pieces
+}
+
+struct Square {
+    piece: Option<ChessPiece>,
 }
