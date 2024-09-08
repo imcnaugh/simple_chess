56d98596a322db2ed2a0bdda343026a85d73c4af
Ian McNaughton <IanMcNaugh@gmail.com>
2024-09-07 21:30:51 -0500

    this needs testing

--- src/better_chess_game.rs
+++ src/better_chess_game.rs
@@ -1,4 +1,5 @@
 use crate::board::Board;
+use crate::board_square::BoardSquare;
 use crate::pieces::better_piece::{ChessPiece, Piece, PieceColor};
 use crate::pieces::better_piece::ChessPiece::*;
 
@@ -6,18 +7,22 @@ struct Game<'a> {
     chess_board: Board<'a>,
     white_pieces: Vec<Piece>,
     black_pieces: Vec<Piece>,
+    current_players_move: PieceColor,
 }
 
 impl<'a> Game<'_> {
     fn new() -> Game<'a> {
-        let board = Board::new(8, 8);
-        let white_pieces = Self::create_pieces(PieceColor::White);
-        let black_pieces = Self::create_pieces(PieceColor::Black);
+        let mut board = Board::new(8, 8);
+        let mut white_pieces = Self::create_pieces(PieceColor::White);
+        let mut black_pieces = Self::create_pieces(PieceColor::Black);
+
+        place_pieces_in_starting_positions(&mut board, &mut white_pieces, &mut black_pieces);
 
         Game {
             chess_board: board,
             white_pieces,
             black_pieces,
+            current_players_move: PieceColor::White,
         }
     }
 
@@ -49,4 +54,13 @@ impl<'a> Game<'_> {
             create_piece(Pawn { _has_moved: false }),
         ]
     }
+}
+
+fn place_pieces_in_starting_positions(board: &mut Board, white_pieces: &mut Vec<Piece>, black_pieces: &mut Vec<Piece>) {
+
+}
+
+fn set_piece_on_space<'a>(piece: &'a mut Piece, space: &mut BoardSquare<'a>) {
+    piece.board_square_id = Some(space.name.clone());
+    space.set_piece(piece);
 }
