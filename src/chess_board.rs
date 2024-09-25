81fa582a18525a6ab013950a5bb7108b6362b4ce
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-09-24 22:25:19 -0500

    why were there so many nested for loops, thanks ai, bah

--- src/chess_board.rs
+++ src/chess_board.rs
@@ -36,8 +36,6 @@ impl GameBoard {
     }
 
     pub fn from_string(width: usize, height: usize, s: &str) -> Result<GameBoard, String> {
-        // validate and sanitize string
-        // remove newlines from string
         let s = s.replace('\n', "");
 
         if s.len() < width * height {
@@ -47,64 +45,22 @@ impl GameBoard {
         let mut board = GameBoard::build(width, height);
 
         for i in 0..(width * height) {
-            let mut index = 0;
-            for i in 0..height {
-                for j in 0..width {
-                    let piece = match &s[index..index + 1] {
-                        "P" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::Pawn,
-                        }),
-                        "R" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::Rook,
-                        }),
-                        "N" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::Knight,
-                        }),
-                        "B" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::Bishop,
-                        }),
-                        "Q" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::Queen,
-                        }),
-                        "K" => Some(ChessPiece {
-                            color: Color::White,
-                            piece_type: PieceType::King,
-                        }),
-                        "p" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::Pawn,
-                        }),
-                        "r" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::Rook,
-                        }),
-                        "n" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::Knight,
-                        }),
-                        "b" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::Bishop,
-                        }),
-                        "q" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::Queen,
-                        }),
-                        "k" => Some(ChessPiece {
-                            color: Color::Black,
-                            piece_type: PieceType::King,
-                        }),
-                        _ => None,
-                    };
-                    board.squares[i * width + j] = piece;
-                    index += 1;
-                }
-            }
+            let piece = match &s[i..i + 1] {
+                "♙" => Some(ChessPiece::new(Color::White, PieceType::Pawn)),
+                "♖" => Some(ChessPiece::new(Color::White, PieceType::Rook)),
+                "♘" => Some(ChessPiece::new(Color::White, PieceType::Knight)),
+                "♗" => Some(ChessPiece::new(Color::White, PieceType::Bishop)),
+                "♕" => Some(ChessPiece::new(Color::White, PieceType::Queen)),
+                "♔" => Some(ChessPiece::new(Color::White, PieceType::Knight)),
+                "♟" => Some(ChessPiece::new(Color::Black, PieceType::Pawn)),
+                "♜" => Some(ChessPiece::new(Color::Black, PieceType::Rook)),
+                "♞" => Some(ChessPiece::new(Color::Black, PieceType::Knight)),
+                "♝" => Some(ChessPiece::new(Color::Black, PieceType::Bishop)),
+                "♛" => Some(ChessPiece::new(Color::Black, PieceType::Queen)),
+                "♚" => Some(ChessPiece::new(Color::Black, PieceType::Knight)),
+                _ => None,
+            };
+            board.squares[i] = piece;
         }
 
         Ok(board)
