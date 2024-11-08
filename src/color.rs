96dda4c80a3524b28f9fa0fa8ea60a86d14bee74
Ian Mcnaughton <IanMcNaugh@gmail.com>
2024-11-08 16:48:09 -0600

    i can tell if a color is in check

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,5 @@
+use crate::Color::{Black, White};
+
 /// Color module
 ///
 /// This module contains the Color enum, which represents the color of either a chess piece or a
@@ -6,4 +8,13 @@
 pub enum Color {
     White,
     Black,
+}
+
+impl Color {
+    pub fn opposite_color(&self) -> Color {
+        match self {
+            White => Black,
+            Black => White
+        }
+    }
 }
--- src/game_analyser.rs
+++ src/game_analyser.rs
@@ -1,10 +1,11 @@
-use crate::{Game, PieceType};
+use crate::{Color, Game, PieceType};
+use crate::chess_board::GameBoard;
 
 pub fn get_legal_moves(game: &Game) {
     let current_turn = game.current_turn;
-    
+
     let mut in_check = false;
-    
+
     for col in 0..8 {
         for row in 0..8 {
             let piece = game.board.check_space(col, row);
@@ -17,3 +18,39 @@ pub fn get_legal_moves(game: &Game) {
     }
 }
 
+fn is_color_in_check(board: &GameBoard, color: Color) -> bool {
+    let opposite_color = color.opposite_color();
+
+    for col in 0 .. board.get_width() {
+        for row in 0 .. board.get_height() {
+            if let Some(piece) = board.check_space(col, row) {
+                if piece.color == opposite_color{
+                    let moves = piece.get_legal_moves(col, row, board);
+                    for mov in moves {
+                        if let Some(takes_piece) = mov.takes {
+                            if takes_piece.get_piece_type() == &PieceType::King &&
+                                takes_piece.get_color() == &color {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::chess_board::GameBoard;
+    use super::*;
+
+    #[test]
+    fn idk() {
+        let board = GameBoard::from_string(2,2, concat!(" ♛\n", "♔ ", )).unwrap();
+        let is_in_check = is_color_in_check(&board, Color::White);
+
+        assert_eq!(true, is_in_check)
+    }
+}
