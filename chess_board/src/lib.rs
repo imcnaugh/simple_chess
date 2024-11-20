684dd27843bf1bd25ef51c6e48a765f06b627e7a
Ian McNaughton <IanMcNaugh@gmail.com>
2024-11-20 17:55:02 -0600

    breaking things into crates

---
+++ chess_board/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod chess_board;
+pub mod chess_board_square;
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-mod chess_board;
-mod chess_board_square;
 pub mod chess_move;
 mod chess_piece;
 pub mod color;
 mod game;
 pub mod game_analyser;
 pub mod game_state;
-
-use chess_board::GameBoard;
 
 pub use chess_piece::{ChessPiece, PieceType};
 pub use color::Color;
