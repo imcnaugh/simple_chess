d26cfdec88fd6fd69aec7761a8fd528c7896c479
Ian McNaughton <IanMcNaugh@gmail.com>
2024-11-24 13:07:20 -0600

    added some helper methods to chess game for printing a board in FEN notation, also started to flush out game as FEN string

--- chess_game/src/chess_game.rs
+++ chess_game/src/chess_game.rs
@@ -1,8 +1,9 @@
 use game_board::Board;
 use crate::Color;
+use crate::piece::ChessPiece;
 
 pub struct ChessGame {
-    board: Board,
+    board: Board<dyn ChessPiece>,
     current_players_turn: Color,
     turn_number: usize,
     fifty_move_rule_counter: usize,
@@ -10,4 +11,64 @@ pub struct ChessGame {
     can_white_castle_long: bool,
     can_black_castle_short: bool,
     can_black_castle_long: bool,
+}
+
+impl ChessGame {
+
+    pub fn new() -> ChessGame {
+        ChessGame {
+            // TODO setup the board in starting position
+            board: Board::<dyn ChessPiece>::build(8, 8).unwrap(),
+            current_players_turn: Color::White,
+            turn_number: 1,
+            fifty_move_rule_counter: 0,
+            can_white_castle_short: true,
+            can_white_castle_long: true,
+            can_black_castle_short: true,
+            can_black_castle_long: true,
+        }
+    }
+
+    /// Get board
+    ///
+    /// # Returns
+    /// `Board<dyn ChessPiece>`: The board in its current state
+    pub fn get_board(&self) -> &Board<dyn ChessPiece> {
+        &self.board
+    }
+
+    
+    /// Returns the color of the player whose turn it is.
+    ///
+    /// # Returns
+    ///
+    /// `Color`: An enum value representing the current player's turn.
+    /// - This can be either `Color::White` or `Color::Black`.
+    ///
+    /// # Examples
+    /// ```
+    /// let chess_game = ChessGame::new();
+    /// // Assuming the game starts with White's turn
+    /// assert_eq!(chess_game.get_current_players_turn(), Color::White);
+    /// ```
+    pub fn get_current_players_turn(&self) -> Color {
+        self.current_players_turn
+    }
+
+
+    /// Get castling rights
+    ///
+    /// Returns a tuple containing four booleans that indicate the castling rights.
+    /// The booleans represent:
+    /// - (can_white_castle_long, can_white_castle_short, can_black_castle_long, can_black_castle_short)
+    ///
+    /// # Returns
+    ///
+    /// (bool, bool, bool, bool): A tuple representing the castling rights for white and black players.
+    pub fn get_castling_rights(&self) -> (bool, bool, bool, bool) {
+        (self.can_white_castle_long,
+        self.can_white_castle_short,
+        self.can_black_castle_long,
+        self.can_black_castle_short)
+    }
 }
--- chess_game/src/encoding/forsyth_edwards_notation.rs
+++ chess_game/src/encoding/forsyth_edwards_notation.rs
@@ -1,12 +1,25 @@
-use game_board::{Board, Piece};
+use game_board::Board;
 use crate::chess_game::ChessGame;
+use crate::Color;
+use crate::piece::ChessPiece;
 
-pub fn encode_game_as_string(game: ChessGame) -> String {
-    
-    format!("")
+pub fn encode_game_as_string(game: &ChessGame) -> String {
+    format!("{} {}",
+            get_board_as_fen_string(game),
+            get_current_turn_char(game)
+    )
 }
 
-fn encode_row(board: Board, row: usize) -> String {
+fn get_board_as_fen_string(game: &ChessGame) -> String {
+    let board = game.get_board();
+
+    let board_as_fen_string: String = (0..board.get_height()).rev()
+        .map(|rank| encode_row(board, rank))
+        .collect().join("/");
+    board_as_fen_string
+}
+
+fn encode_row(board: &Board<dyn ChessPiece>, row: usize) -> String {
     let mut result = String::new();
     
     let mut empty_space_counter: usize = 0;
@@ -17,10 +30,27 @@ fn encode_row(board: Board, row: usize) -> String {
                 result.push_str(&empty_space_counter.to_string());
                 empty_space_counter = 0;
             }
-            result.push(piece.);
+            result.push(piece.get_fen_char());
         } else {
             empty_space_counter+=1;
         }
+        if empty_space_counter != 0 {
+            result.push_str(&empty_space_counter.to_string());
+        }
     }
     result
+}
+
+fn get_current_turn_char(game: &ChessGame) -> char {
+    let current_turn = match game.get_current_players_turn() {
+        Color::White => 'w',
+        Color::Black => 'b',
+    };
+    current_turn
+}
+
+fn get_castling_rights(game: &ChessGame) -> String {
+    let mut result = String::new();
+
+    if game.
 }
