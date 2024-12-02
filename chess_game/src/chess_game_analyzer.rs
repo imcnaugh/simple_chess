6556f19b22870e9dd1070599b44362f53cc458b9
Ian McNaughton <IanMcNaugh@gmail.com>
2024-12-01 19:29:31 -0600

    more tests, its looking good

--- chess_game/src/chess_game_analyzer.rs
+++ chess_game/src/chess_game_analyzer.rs
@@ -21,7 +21,7 @@ pub fn get_legal_moves(mut game: ChessGame) -> Vec<ChessMoveType> {
         .collect::<Vec<ChessMoveType>>()
 }
 
-fn is_in_check(color: Color, board: &Board<ChessPiece>, last_move: Option<&ChessMoveType>) -> bool {
+pub fn is_in_check(color: Color, board: &Board<ChessPiece>, last_move: Option<&ChessMoveType>) -> bool {
     for row in 0..board.get_height() {
         for col in 0..board.get_width() {
             if let Some(piece) = board.get_piece_at_space(col, row) {
@@ -73,15 +73,16 @@ fn get_all_moves_for_color(
 #[cfg(test)]
 mod tests {
     use crate::ChessMoveType::Move;
-    use crate::Color::White;
-    use crate::piece::PieceType::{Knight, Pawn};
+    use crate::codec::forsyth_edwards_notation::build_game_from_string;
+    use crate::Color::{Black, White};
+    use crate::piece::PieceType::{Bishop, Knight, Pawn, Queen, Rook};
     use super::*;
 
     #[test]
     fn get_legal_moves_for_starting_position() {
         let game = ChessGame::new();
         let legal_moves = get_legal_moves(game);
-        assert_eq!(legal_moves.len(), 20);
+        assert_eq!(20, legal_moves.len());
 
         for col in 0..8 {
             for pawn_move_length in 1..=2 {
@@ -128,5 +129,92 @@ mod tests {
         assert!(legal_moves.contains(&knight_c3));
         assert!(legal_moves.contains(&knight_f3));
         assert!(legal_moves.contains(&knight_h3));
+    }
+
+    #[test]
+    fn player_in_checkmate_has_no_legal_moves() {
+        let game = build_game_from_string("k6R/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(legal_moves.len(), 0);
+    }
+
+    #[test]
+    fn king_in_check_limits_legal_moves() {
+        let game = build_game_from_string("k6R/1ppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(1, legal_moves.len());
+        let expected_move = Move {
+            original_position: (0, 7),
+            new_position: (0, 6),
+            piece: ChessPiece::new(King, Black),
+            taken_piece: None,
+            promotion: None,
+        };
+        assert_eq!(expected_move, legal_moves[0])
+    }
+
+    #[test]
+    fn more_complex_checkmate() {
+        let game = build_game_from_string("8/8/8/8/2n5/1p5r/K7/BB6 w - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(0, legal_moves.len());
+    }
+
+    #[test]
+    fn pieces_can_be_pinned_to_the_king() {
+        let game = build_game_from_string("K2B3r/8/8/8/8/8/8/8 w - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(3, legal_moves.len());
+        let make_king_moves = |new_position| -> ChessMoveType {
+            Move {
+                original_position: (0,7),
+                new_position,
+                piece: ChessPiece::new(King, White),
+                taken_piece: None,
+                promotion: None,
+            }
+        };
+        [(1,7), (1,6), (0,6)].iter().for_each(|new_position| {
+            let m = make_king_moves(*new_position);
+            assert!(legal_moves.contains(&m));
+        })
+    }
+
+    #[test]
+    fn pawns_can_promote() {
+        let game = build_game_from_string("8/4P3/8/8/8/8/8/8 w - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(4, legal_moves.len());
+        [Queen, Rook, Bishop, Knight].iter().for_each(|promotion_option| {
+            let expected_move = Move {
+                original_position: (4, 6),
+                new_position: (4, 7),
+                piece: ChessPiece::new(Pawn, White),
+                taken_piece: None,
+                promotion: Some(ChessPiece::new(*promotion_option, White)),
+            };
+            assert!(legal_moves.contains(&expected_move));
+        })
+    }
+
+    #[test]
+    fn cannot_en_passant_if_pawn_pinned() {
+        let game = build_game_from_string("2r5/8/8/2Pp4/8/8/8/2K5 w - d6 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(6, legal_moves.len());
+    }
+
+    #[test]
+    fn can_en_passant() {
+        let game = build_game_from_string("8/8/8/2Pp4/8/8/8/2K5 w - d6 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(7, legal_moves.len());
+    }
+
+    #[test]
+    fn stalemate_should_yield_no_legal_moves() {
+        let game = build_game_from_string("1r4b1/8/8/8/8/8/8/K7 w - - 0 1").unwrap();
+        let legal_moves = get_legal_moves(game);
+        assert_eq!(0, legal_moves.len());
     }
 }
