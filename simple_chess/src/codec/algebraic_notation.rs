c707a2b57fb93b583a4cadef9976dd511ef729a7
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-11-24 21:12:46 -0600

    finding conflicts

--- simple_chess/src/codec/algebraic_notation.rs
+++ simple_chess/src/codec/algebraic_notation.rs
@@ -1,8 +1,8 @@
-use game_board::Board;
-use crate::{ChessGame, ChessMoveType, Color};
 use crate::chess_game_state_analyzer::GameState;
 use crate::codec::long_algebraic_notation::encode_move_as_long_algebraic_notation;
 use crate::piece::ChessPiece;
+use crate::{ChessGame, ChessMoveType};
+use game_board::Board;
 
 pub fn encode_move_as_algebraic_notation(
     chess_move_type: &ChessMoveType,
@@ -10,33 +10,91 @@ pub fn encode_move_as_algebraic_notation(
 ) -> String {
     match chess_move_type {
         ChessMoveType::Move {
-            original_position, new_position, piece, taken_piece, promotion
+            original_position,
+            new_position,
+            piece,
+            taken_piece,
+            promotion,
         } => encode_move_as_long_algebraic_notation(chess_move_type),
         ChessMoveType::Castle { .. } => encode_move_as_long_algebraic_notation(chess_move_type),
         ChessMoveType::EnPassant {
-            original_position, new_position, piece, taken_piece, taken_piece_position, promotion
+            original_position,
+            new_position,
+            piece,
+            taken_piece,
+            taken_piece_position,
+            promotion,
         } => encode_move_as_long_algebraic_notation(chess_move_type),
     }
 }
 
 fn idk(chess_move_type: &ChessMoveType, resulting_position: &Board<ChessPiece>) {
-    let moving_piece_color = match chess_move_type {
-        ChessMoveType::Move { piece, .. } => piece.get_color(),
-        ChessMoveType::EnPassant { piece, .. } => piece.get_color(),
-        _ => panic!("Unexpected move type")
+    let (
+        moving_piece_color,
+        moving_piece_type,
+        moving_piece_original_location,
+        moving_piece_new_position,
+    ) = match chess_move_type {
+        ChessMoveType::Move {
+            piece,
+            original_position,
+            new_position,
+            ..
+        } => (
+            piece.get_color(),
+            piece.get_piece_type(),
+            original_position,
+            new_position,
+        ),
+        ChessMoveType::EnPassant {
+            piece,
+            original_position,
+            new_position,
+            ..
+        } => (
+            piece.get_color(),
+            piece.get_piece_type(),
+            original_position,
+            new_position,
+        ),
+        _ => panic!("Unexpected move type"),
     };
 
     let mut game = ChessGame::build(
-        resulting_position.clone(), moving_piece_color.opposite(), 1, 0, true, true, true, true, vec![chess_move_type.clone()]);
+        resulting_position.clone(),
+        moving_piece_color.opposite(),
+        1,
+        0,
+        true,
+        true,
+        true,
+        true,
+        vec![chess_move_type.clone()],
+    );
 
     game.undo_last_move();
 
     let previous_state = &game.get_game_state();
     let previous_legal_moves = match previous_state {
-        GameState::InProgress { legal_moves, .. } => {legal_moves}
-        GameState::Check { legal_moves, .. } => {legal_moves}
-        _ => panic!("Unexpected state")
+        GameState::InProgress { legal_moves, .. } => legal_moves,
+        GameState::Check { legal_moves, .. } => legal_moves,
+        _ => panic!("Unexpected state"),
     };
 
-
+    let conflicts = previous_legal_moves
+        .iter()
+        .filter(|&plm| match plm {
+            ChessMoveType::Move {
+                piece,
+                original_position,
+                new_position,
+                ..
+            } => {
+                piece.get_piece_type() == moving_piece_type
+                    && original_position != moving_piece_original_location
+                    && new_position == moving_piece_new_position
+            }
+            _ => false,
+        })
+        .collect::<Vec<&ChessMoveType>>();
 }
--- simple_chess/src/codec/mod.rs
+++ simple_chess/src/codec/mod.rs
@@ -1,4 +1,4 @@
-pub mod long_algebraic_notation;
+mod algebraic_notation;
 pub mod binary;
 pub mod forsyth_edwards_notation;
-mod algebraic_notation;
+pub mod long_algebraic_notation;
